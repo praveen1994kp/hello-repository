21e9a4ecb13f6092f49962dee8d39946a7627f53
Praveen <praveen1994kp@gmail.com>
2018-01-07 01:09:59 +0530

    Initial Commit

---
+++ hello-world/src/main/java/com/example/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		sayHello("Harry");
+
+	}
+	
+	private static String sayHello(String name) {
+		return "Hello "+name;
+	}
+
+}
